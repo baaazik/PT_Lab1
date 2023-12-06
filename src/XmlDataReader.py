94d90055f0b634b87d668b0710e21527d9224692
Zabaznova Anzhelika Evgenyevna <Zabaznova2303@gmail.com>
2023-12-06 06:15:18 +0300

    Реализован парсер XML и тест

--- src/XmlDataReader.py
+++ src/XmlDataReader.py
@@ -10,8 +10,22 @@ class XmlDataReader(DataReader):
         self.students: DataType = {}
 
     def read(self, path: str) -> DataType:
+        # Парсим XML
         document = ET.parse(path)
         root = document.getroot()
-        for elem in root.iter():
-            print(elem.tag)
+
+        # Проходим по всем студентам в файле
+        for root_child in root:
+            if root_child.tag == "student":
+                student_name = root_child.attrib["name"]
+                subjects = []
+
+                # Проходим по всем предметам в студенте
+                for student_child in root_child:
+                    if student_child.tag == "subject":
+                        subject_name = student_child.attrib["name"]
+                        mark = int(student_child.attrib["mark"])
+                        subjects.append((subject_name, mark))
+
+                self.students[student_name] = subjects
         return self.students
--- test/test_XmlDataReader.py
+++ test/test_XmlDataReader.py
@@ -7,38 +7,32 @@ from src.XmlDataReader import XmlDataReader
 class TestTextDataReader:
     @pytest.fixture()
     def file_and_data_content(self) -> tuple[str, DataType]:
-        text = \
-"""
-<?xml version="1.0" encoding="UTF-8" ?>
+        text = """<?xml version="1.0" encoding="UTF-8" ?>
 <root>
-    <Иванов Константин Дмитриевич>
-        <математика>91</математика>
-        <химия>100</химия>
-    </Иванов Константин Дмитриевич>
-    <Петров Петр Семенович>
-        <русский язык>87</русский язык>
-        <литература>78</литература>
-    </Петров Петр Семенович>
+    <student name="Иванов Константин Дмитриевич">
+        <subject name="математика" mark="91"/>
+        <subject name="химия" mark="100"/>
+    </student>
+    <student name="Петров Петр Семенович">
+        <subject name="русский язык" mark="87"/>
+        <subject name="литература" mark="78"/>
+    </student>
 </root>
 """
         data = {
-            "Иванов Константин Дмитриевич": [
-                ("математика", 91), ("химия", 100)
-            ],
-            "Петров Петр Семенович": [
-                ("русский язык", 87), ("литература", 78)
-            ]
+            "Иванов Константин Дмитриевич": [("математика", 91), ("химия", 100)],
+            "Петров Петр Семенович": [("русский язык", 87), ("литература", 78)],
         }
 
         return text, data
 
     @pytest.fixture()
-    def filepath_and_data(self,
-                          file_and_data_content: tuple[str, DataType],
-                          tmpdir) -> tuple[str, DataType]:
+    def filepath_and_data(
+        self, file_and_data_content: tuple[str, DataType], tmpdir
+    ) -> tuple[str, DataType]:
 
         p = tmpdir.mkdir("datadir").join("my_data.txt")
-        p.write_text(file_and_data_content[0], encoding='utf-8')
+        p.write_text(file_and_data_content[0], encoding="utf-8")
         return str(p), file_and_data_content[1]
 
     def test_read(self, filepath_and_data: tuple[str, DataType]) -> None:
