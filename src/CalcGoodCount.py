89dd45c88da3f0e15798db345495ab33bb3bb885
Zabaznova Anzhelika Evgenyevna <Zabaznova2303@gmail.com>
2023-12-06 04:48:13 +0300

    Задание 4: расчёт по индивидуальному варианту
    Расчёт количества хорошистов

---
+++ src/CalcGoodCount.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from Types import DataType
+
+
+class CalcGoodCount:
+    def __init__(self, data: DataType) -> None:
+        self.data: DataType = data
+        self.count = 0
+
+    def calc(self) -> int:
+        good_count = 0
+
+        for key in self.data:
+            is_student_good = True
+
+            for subject in self.data[key]:
+                if subject[1] < 76:
+                    is_student_good = False
+
+            if is_student_good:
+                good_count += 1
+
+        self.count = good_count
+        return self.count
